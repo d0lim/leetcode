6bdb7a7cd3c1bcd67b919e6712a4a2b8d5af45ab
D0Lim <limdongyoung0@gmail.com>
2024-09-11 21:41:22 +0900

    refactor: 189-rotate-array

--- solutions/189-rotate-array.py
+++ solutions/189-rotate-array.py
@@ -3,8 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        copied = [n for n in nums]
-        length = len(copied)
-        
-        for i in range(length):
-            nums[i] = copied[(i - k) % length]
+        k %= len(nums)
+
+        def reverse(left, right):
+            while left < right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
+            
+        reverse(0, len(nums) - 1)
+        reverse(0, k - 1)
+        reverse(k, len(nums) - 1)
