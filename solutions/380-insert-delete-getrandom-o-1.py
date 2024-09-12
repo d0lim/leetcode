a5a7ee9aea7a54ae5cff120a4338a61b7cfed655
D0Lim <limdongyoung0@gmail.com>
2024-09-12 22:57:39 +0900

    refactor: 380-insert-delete-getrandom-o-1

--- solutions/380-insert-delete-getrandom-o-1.py
+++ solutions/380-insert-delete-getrandom-o-1.py
@@ -1,23 +1,25 @@
-from collections import defaultdict
-import random
-
 class RandomizedSet:
-    dic = defaultdict(bool)
 
     def __init__(self):
-        self.dic = defaultdict(bool)
+        self.ran = set()
+        
 
     def insert(self, val: int) -> bool:
-        if not self.dic[val]:
-            self.dic[val] = True
+        if val not in self.ran:
+            self.ran.add(val)
             return True
-        return False
+        else:
+            return False
+        
 
     def remove(self, val: int) -> bool:
-        if self.dic[val]:
-            self.dic[val] = False
+        if val in self.ran:
+            self.ran.remove(val)
             return True
-        return False
+        else:
+            return False
+
+        
 
     def getRandom(self) -> int:
-        return list(random.choice(list(filter(lambda x: x[1] == True, self.dic.items()))))[0]
+        return random.choice(list(self.ran))
