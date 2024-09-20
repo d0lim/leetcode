cb9a18ec4be4dff495a64db1983331c7c7abda41
D0Lim <limdongyoung0@gmail.com>
2024-09-20 21:02:36 +0900

    refactor: 12-integer-to-roman

--- solutions/12-integer-to-roman.py
+++ solutions/12-integer-to-roman.py
@@ -3,43 +3,30 @@ class Solution:
         result = ''
         place = 1
         while num >= 1:
-            result = self.convertDigitToRoman(num % 10, place) + result
+            result = self.convertPlaceDigitToRoman(num % 10, place) + result
             place += 1
             num //= 10
         return result
 
     # digit is between 0 to 9
     # place is between 1 to 3 for this problem
-    def convertDigitToRoman(self, digit: int, place: int) -> str:
+    def convertPlaceDigitToRoman(self, digit: int, place: int) -> str:
         if place == 4:
             return 'M' * digit
         elif place == 3:
-            if digit == 9:
-                return 'CM'
-            elif digit == 4:
-                return 'CD'
-            else:
-                if digit >= 5:
-                    return 'D' + 'C' * (digit - 5)
-                else:
-                    return 'C' * digit
+            return self.convertDigitToRoman(digit, 'M', 'D', 'C')
         elif place == 2:
-            if digit == 9:
-                return 'XC'
-            elif digit == 4:
-                return 'XL'
-            else:
-                if digit >= 5:
-                    return 'L' + 'X' * (digit - 5)
-                else:
-                    return 'X' * digit
+            return self.convertDigitToRoman(digit, 'C', 'L', 'X')
         elif place == 1:
-            if digit == 9:
-                return 'IX'
-            elif digit == 4:
-                return 'IV'
+            return self.convertDigitToRoman(digit, 'X', 'V', 'I')
+    
+    def convertDigitToRoman(self, digit: int, symbolForTen: str, symbolForFive: str, symbolForOne: str)-> str:
+        if digit == 9:
+            return symbolForOne + symbolForTen
+        elif digit == 4:
+            return symbolForOne + symbolForFive
+        else:
+            if digit >= 5:
+                return symbolForFive + symbolForOne * (digit -5)
             else:
-                if digit >= 5:
-                    return 'V' + 'I' * (digit - 5)
-                else:
-                    return 'I' * digit
+                return symbolForOne * digit
