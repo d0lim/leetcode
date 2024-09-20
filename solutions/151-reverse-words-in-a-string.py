ac858cb0d174499e0d110c1f7ba0f00819e35935
D0Lim <limdongyoung0@gmail.com>
2024-09-20 21:17:35 +0900

    refactor: 151-reverse-words-in-a-string

--- solutions/151-reverse-words-in-a-string.py
+++ solutions/151-reverse-words-in-a-string.py
@@ -1,3 +1,3 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        return ' '.join(reversed(list(filter(lambda x: len(x) > 0, s.split(' ')))))
+        return ' '.join(reversed(s.split()))
